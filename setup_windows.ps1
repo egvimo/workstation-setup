d5ab3f9f14bf5d52a15b8c14834cc2bb31f0986c
egvimo <egvimo@gmail.com>
2021-03-24 17:44:50 +0100

    Fix setup scripts

--- setup_windows.ps1
+++ setup_windows.ps1
@@ -1,8 +1,6 @@
 # Install WSL and restart (if necessary)
 if (-Not (Get-Command wsl -ErrorAction SilentlyContinue))
 {
-  #dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart
-  #dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart
   Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
   Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform -NoRestart
 
@@ -12,14 +10,7 @@ if (-Not (Get-Command wsl -ErrorAction SilentlyContinue))
 # Install Ubuntu
 Invoke-WebRequest -Uri https://aka.ms/wslubuntu2004 -OutFile Ubuntu2004.appx -UseBasicParsing
 Add-AppxPackage .\Ubuntu2004.appx
-
-# Start WinRM Service
-#Set-Service -Name "WinRM" -StartupType Automatic
-#Start-Service -Name "WinRM" -ErrorAction Stop
+Remove-Item .\Ubuntu2004.appx
 
 # Enable Remoting (optinal: -SkipNetworkProfileCheck for public zones)
 Enable-PSRemoting -Force -ErrorAction Stop
-
-#winrm set winrm/config/service @{AllowUnencrypted="false"}
-#winrm set winrm/config/service/auth @{Basic="false"}
-#winrm set winrm/config/service/auth @{Kerberos="true"}
--- setup_wsl.sh
+++ setup_wsl.sh
@@ -1,6 +1,10 @@
-#!/bin/sh
+#!/bin/bash
+set -e
 
-sudo apt-get install python3-dev libkrb5-dev krb5-user
+sudo apt update
+sudo apt upgrade -y
+sudo apt dist-upgrade -y
+sudo apt install -y python3-pip python3-venv python3-dev libkrb5-dev krb5-user
 
 python3 -m venv .venv --prompt ansible
 
